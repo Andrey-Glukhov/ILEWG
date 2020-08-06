a8a024d4f45e7f2aa246208066a1b2c90cfd7794
Eliza <gluhova.liza@gmail.com>
2020-08-06 12:41:14 +0200

    about changes

--- about.php
+++ about.php
@@ -12,10 +12,19 @@ get_header(); ?>
       <div class="col-12 order-md-12 order-sm-1 order-1 about_header">
         <h1>ABOUT ILEWG</h1>
       </div>
+    </div>
+    <div class="row justify-content-center">
+      <?php $about_ilewg = new WP_Query(array( 'page_id' => 2 ));
+if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
+
+      <div class="col-md-8 col-sm-12"><?php the_content(); ?></div>
+  <?php
+    endwhile;
+    endif
+  ?>
 
     </div>
-
 </div>
 </div>
 
